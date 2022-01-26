e8601c02adb7d9da6b26e1235444d1fd5a9f5e50
ChrisB9472 <93794387+ChrisB9472@users.noreply.github.com>
2022-01-26 16:29:40 -0500

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,25 +1,67 @@
 import React from 'react';
-import { View, Text, Button, TextInput  } from 'react-native';
+import { View, Platform, KeyboardAvoidingView, Text, Button, TextInput  } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat'
+
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: ""};
+    this.state = { messages: [],};
   }
+  componentDidMount() {
+    // get username prop from Start.js
+    let { name } = this.props.route.params;
+    this.props.navigation.setOptions({ title: name });
+
+    
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: 'This is a system message',
+          createdAt: new Date(),
+          system: true,
+         },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+  
+
+  
 
   render() {
     //let name = this.props.route.params.name; 
       //this.props.navigation.setOptions({ title: name});
 
-    
+      
 
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
-        <Button 
-          title="Go to Start"
-          onPress={() => this.props.navigation.navigate("Start")}
-          />
-      </View>
+      <View style={{ flex: 1}}>
+      <GiftedChat
+      messages={this.state.messages}
+      onSend={messages => this.onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />{ Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+  }
+    </View>
     );
   };
 }
